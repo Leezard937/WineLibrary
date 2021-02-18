f628920a6d608189dfcb0ee6eb32022e0b4c0db7
Leezard937 <79282953+Leezard937@users.noreply.github.com>
2021-02-18 22:28:45 +0200

    v1

---
+++ js/main-dist.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//slick-slider init
+$('.main-header__slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 400,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  useCSS: false
+});
